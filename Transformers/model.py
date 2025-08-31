69e90eb9e8b7b40a746892b35ae65afe34cae557
SimoneGiaco <gcmsmn@gmail.com>
2025-08-31 16:37:28 +0200

    Update model.py

--- Transformers/model.py
+++ Transformers/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import math
 
 
-#Embedding vettoriale dei tokens
+#Vector Embedding for the tokens
 class InputEmbedding(nn.Module):
     def __init__(self,d_model: int, vocab_size: int):
         super().__init__()
@@ -14,7 +14,7 @@ class InputEmbedding(nn.Module):
     def forward(self,x):
         return math.sqrt(self.d_model)*self.embedding(x)
 
-#Vettori posizione
+#Position vectors to be added to the tokens in the sequence
 class PositionalEncoding(nn.Module):
     def __init__(self,d_model: int,seq_len: int, dropout: float):
         super().__init__()
@@ -22,15 +22,15 @@ class PositionalEncoding(nn.Module):
         self.seq_len=seq_len
         self.dropout=nn.Dropout(dropout)
         
-        #Costruiamo la matrice per i vettori posizione (seq_len,d_model)
+        #We construct the (seq_len,d_model) positional matrix
         pe=torch.zeros(seq_len,d_model)
         position=torch.arange(0,seq_len,dtype=torch.float).unsqueeze(1)
         div_term=torch.exp(torch.arange(0,d_model,2).float()*(-math.log(10000.0)/d_model))
         #Popoliamo la matrice pe. Si usa sin per le colonne pari e cos per quelle dispari
         pe[:,0::2]=torch.sin(position*div_term)
         pe[:,1::2]=torch.cos(position*div_term)
-        pe=pe.unsqueeze(0) # tensore di forma (1,seq_len,d_model)
-        #self.pe=pe
+        pe=pe.unsqueeze(0) # tensor of shape (1,seq_len,d_model)
+        #self.pe=pe We register on the buffer since we do not need to compute gradients for pe.
         self.register_buffer('pe', pe)
         
     def forward(self,x):
@@ -38,8 +38,9 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
-#Strati per il modulo Encoder
-#Normalizzazione
+#Layers for the Encoder Block
+
+#Normalization Layer
 class LayerNorm(nn.Module):
     def __init__(self,eps: float=10**-6):
         super().__init__()
@@ -52,7 +53,7 @@ class LayerNorm(nn.Module):
         std=x.std(dim=-1, keepdim=True)
         return self.alpha*(x-mean)/(std+self.eps) + self.bias
 
-# Blocco FeedForward
+#FeedForward block
 class FeedForwardBlock(nn.Module):
     def __init__(self,d_model: int, d_ff: int, dropout: float):
         super().__init__()
@@ -63,7 +64,7 @@ class FeedForwardBlock(nn.Module):
     def forward(self,x):
         return self.linear2(self.dropout(torch.relu(self.linear1(x))))
 
-#Multi-head Attention
+#Multi-head Attention layer
 class MultiHeadAttention(nn.Module):
     def __init__(self,d_model: int,h: int,dropout: float):
         super().__init__()
@@ -78,7 +79,7 @@ class MultiHeadAttention(nn.Module):
         self.w_v=nn.Linear(d_model,d_model,bias=False)
         self.w_o=nn.Linear(d_model,d_model,bias=False)
 
-    #Attention function in staticmethod (può essere chiamata fuori da un'istanza MultiheadAttention)
+    #Attention function in staticmethod (we can call it outside a MultiheadAttention instance. We will need it also later for the Decoder)
     @staticmethod
     def attention(query,key,value,mask,dropout: nn.Dropout):
         d_k=query.shape[-1]
@@ -95,7 +96,7 @@ class MultiHeadAttention(nn.Module):
         key=self.w_k(k)
         value=self.w_v(v)
         
-        #Dividiamo ciascuna in h matrici che hanno ultima dimensione d_k
+        #We divide query,key,value into h heads each of dimension d_k
         query=query.view(query.shape[0],query.shape[1],self.h,self.d_k).transpose(1,2)
         key=key.view(key.shape[0],key.shape[1],self.h,self.d_k).transpose(1,2)
         value=value.view(value.shape[0],value.shape[1],self.h,self.d_k).transpose(1,2)
@@ -114,7 +115,7 @@ class ResidualConnection(nn.Module):
     def forward(self,x,sublayer):
         return x+self.dropout(sublayer(self.norm(x)))
 
-#Combiniamo gli ingredienti sopra nell'architettura dell'Encoder
+#We now combine the above layers into the Encoder block
 class EncoderBlock(nn.Module):
     def __init__(self,self_attention_block: MultiHeadAttention,feed_forward_block: FeedForwardBlock,dropout: float):
         super().__init__()
@@ -127,7 +128,7 @@ class EncoderBlock(nn.Module):
         x=self.res_con[1](x, lambda x: self.f_f(x))
         return x
 
-#Iteriamo n volte il EncoderBlock
+#The encoder is obtained by iterating n times EncoderBlock. We implement this in the Encoder function by passing the list 'layers' as a parameter
 class Encoder(nn.Module):
     def __init__(self, layers: nn.ModuleList):
         super().__init__()
@@ -140,7 +141,7 @@ class Encoder(nn.Module):
         return self.norm(x)
     
 
-#Strati per il modulo Decoder e strato finale (projection layer)
+#Layers for the Decoder block and final layer of the model (projection layer)
 class DecoderBlock(nn.Module):
     def __init__(self,self_attention: MultiHeadAttention,cross_attention: MultiHeadAttention,feed_forward_block: FeedForwardBlock,dropout: float):
         super().__init__()
@@ -155,6 +156,7 @@ class DecoderBlock(nn.Module):
         x=self.res_con[2](x, lambda x: self.f_f(x))
         return x
 
+#Again, as for the Encoder we construct the decoder by passing a list of layers as an argument
 class Decoder(nn.Module):
     def __init__(self, layers: nn.ModuleList):
         super().__init__()
@@ -166,7 +168,7 @@ class Decoder(nn.Module):
             x=layer(x,y,src_mask,tgt_mask)
         return self.norm(x)
 
-#Strato finale
+#Final layer, i.e. Projection
 class ProjectionLayer(nn.Module):
     def __init__(self,d_model: int,vocab_size: int):
         super().__init__()
@@ -176,7 +178,7 @@ class ProjectionLayer(nn.Module):
         return nn.Softmax(dim=-1)(self.linear(x)) 
     
 
-#Architettura Transformer
+#We build the Transformer architecture defining the methods encode, decode and projection.
 class Transformer(nn.Module):
     def __init__(self,src_embedding: InputEmbedding,tgt_embedding: InputEmbedding,src_position: PositionalEncoding,tgt_position: PositionalEncoding,encoder: Encoder,decoder: Decoder,projection: ProjectionLayer):
         super().__init__()
@@ -201,7 +203,7 @@ class Transformer(nn.Module):
     def projection(self,x):
         return self.proj(x) 
     
-#Funzione che mette insieme tutto
+#We define the main functions which builds the model. This is the function we will call in the 'Train.py' file
 def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int=512, dropout: float=0.1,N: int=6, h: int=8) -> Transformer:
     #Embedding layers
     src_embedding=InputEmbedding(d_model,src_vocab_size)
@@ -209,6 +211,7 @@ def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int
     #Position Layers
     src_position=PositionalEncoding(d_model,src_seq_len,dropout)
     tgt_position=PositionalEncoding(d_model,tgt_seq_len,dropout)
+    
     #Encoder Layers
     blocks=[]
     for _ in range(N):
@@ -216,6 +219,7 @@ def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int
         feed_for_block=FeedForwardBlock(d_model,4*d_model,dropout)
         block=EncoderBlock(self_a_block,feed_for_block,dropout)
         blocks.append(block)
+        
     #Decoder Layers
     blocks2=[]
     for _ in range(N):
@@ -224,15 +228,20 @@ def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int
         feed_for_block=FeedForwardBlock(d_model,4*d_model,dropout)
         block=DecoderBlock(self_a_block,cross_a_block,feed_for_block,dropout)
         blocks2.append(block)
-    #Costruiamo Encoder e Decoder
+        
+    #Let us construct Encoder and Decoder by passing as argument the list of layers 'blocks' and 'blocks2'
     encoder=Encoder(nn.ModuleList(blocks))
     decoder=Decoder(nn.ModuleList(blocks2))
+    
     #Projection Layers
     projection=ProjectionLayer(d_model,tgt_vocab_size)
-    #Facciamo il Transformer
+    
+    #Finally we instantiate the Transformer class
     transformer=Transformer(src_embedding,tgt_embedding,src_position,tgt_position,encoder,decoder,projection)
-    #Inizializzazione Parametri
+    
+    #We initialize the parameters of 'transformer' with the Xavier method
     for p in transformer.parameters():
         if p.dim()>1:
             nn.init.xavier_uniform_(p)
+
     return transformer
